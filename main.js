ffecfb1469cfec272dd37a864ed620517f05d08c
keenset1 <laurent14511@gmail.com>
2023-02-07 21:35:59 -0500

    hey

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 // 
 
-const apiKey = "af7eeacdd5fdbdce44752870fb9f569a0";
+const apiKey = "aec8cb2d5254ac2009014232f0e3494e";
 const submitBTN = document.querySelector("#submit-btn");
 const locationInput = document.querySelector("#location");
 
 submitBTN.addEventListener("click", function(event){
     event.preventDefault();
     const location = locationInput.value;
-    const url = "api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={API key}";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`
 
 
     fetch(url)
     .then(response => response.json())
     .then(data=> {
         //convert Kelvin to Fahrenheit
+        console.log(data)
         const high = (32 + (data.main.temp_max -273.15) *9/5).toFixed(1) + '°F';
         const lOW = (32 + (data.main.temp_mIN -273.15) *9/5).toFixed(1) + '°F';
         const forecast = data.weather[0].descrption;
